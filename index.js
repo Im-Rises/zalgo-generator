303c1adeded8183e6cfa766504e492398fc6e90a
Im-Rises <quentin-morel88@hotmail.com>
2023-05-25 00:47:41 -0400

    wip

---
+++ index.js
@@ -0,0 +1,3 @@
+// import {} from './src/zalgo-generator.js';
+
+console.log('Hello, World!');
--- index.ts
+++
@@ -1,4 +0,0 @@
-import {addZalgoToString, unzalgoStringUpMidDown} from "./src/zalgo-generator";
-
-console.log(addZalgoToString('Hello World!', 10));
-
